ee388431d46aa3dbc45f5472c124d03ade953f46
vantezzen <properly@protonmail.com>
2019-02-24 17:02:33 +0100

    Fix Nav not being wide enough

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 export default class Nav extends Component {
     render() {
         return (
-            <nav className="header flex items-center justify-between flex-wrap p-6">
+            <nav className="header flex items-center justify-between flex-wrap p-6 w-full">
                 <div />
                 <Link to="/" className="no-underline">
                     <span className="font-light">minimal</span><span className="font-normal">pedia</span>
